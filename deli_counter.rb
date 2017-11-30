4a6b83d94234d0511ca4443ddf2808d1458e4c92
Dennis Gonzalez <gonzalezdennis6@gmail.com>
2017-11-30 17:50:14 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,6 +5,6 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else katz_deli.each_with_index { |name, index| line += "#{index}. #{name}"}
-    line
+    puts line
   end
 end
